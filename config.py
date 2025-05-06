9dd1ed292c9a9f2b5c052faf3038fa813a46243b
JPZU <juanpablozp@icloud.com>
2025-05-06 13:02:34 -0500

    Escalar el monolito

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
-SECRET_KEY = 'secretkey'
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
